8251f6215051322e0686527baf938550caabce8f
baconbits1204 <rafaelhermoso1204@gmail.com>
2019-09-14 13:40:22 -0400

    initializing firebase

---
+++ assets/js/js.js
@@ -0,0 +1,13 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyD1HsENlptojDC_1zfhfoJiGpGmFE_gyDU",
+    authDomain: "hermosohydroponics.firebaseapp.com",
+    databaseURL: "https://hermosohydroponics.firebaseio.com",
+    projectId: "hermosohydroponics",
+    storageBucket: "",
+    messagingSenderId: "579228589027",
+    appId: "1:579228589027:web:fb7cf3b6ab2789fb719ed6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
